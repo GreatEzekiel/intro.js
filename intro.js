a02604a6e19cbb6b487fc52e5fa536f3633907ad
greatezekiels@gmail.com <mac@Macs-MacBook-Pro.local>
2026-09-08 04:21:27 +0100

    Add initial intro.js script

---
+++ intro.js
@@ -0,0 +1,9 @@
+// intro.js
+
+const name = "Adejare Ezekiel";
+const age = 25;
+const favouriteLanguage = "JavaScript";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favourite Programming Language: ${favouriteLanguage}`);
